b65f03108508171d2be5fa7bcc127df02574d1d7
subum12345 <subum12345@gmail.com>
2022-12-07 18:13:48 +0900

    SPA

--- SPA/server.js
+++ SPA/server.js
@@ -3,7 +3,16 @@ const path = require("path");
 
 const app = express();
 
-// SPA이기 때문에 모든 경로에서 index.html을 불러온다
+app.use(
+  "/static",
+  // express.static -> 정적 파일을 제공하기 위한 express 기본 미들웨어 함수
+  express.static(path.resolve(__dirname, "frontend", "static"))
+);
+
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve("frontend", "index.html"));
+});
+
+app.listen({ port: 4000 }, () => {
+  console.log("http://localhost:4000");
 });
